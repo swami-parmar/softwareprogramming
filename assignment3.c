d176dd01a5d6dd8452ee76b514d43b169d34031f
swami-parmar <swamiparmar2009@gmail.com>
2026-06-10 11:31:56 +0530

    Add files via upload

---
+++ assignment3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+    int team;
+    printf("enter team number(1 for MI):");
+    scanf("%d",&team);
+    if(team ==1){
+        printf("mumbai indians:duniya hila denge\n");
+    }
+    return 0;
+    
+}
